b7f28f7274e6e1e20019e7c31c73667e5206d21f
Xxiled <93330009+xxiled-plastic-cat@users.noreply.github.com>
2024-07-04 13:01:30 +0100

    completed test runs against non-oracle pricing

--- contracts/projects/contracts/__test__/CompXStaking.test.ts
+++ contracts/projects/contracts/__test__/CompXStaking.test.ts
@@ -1,6 +1,7 @@
 import { describe, test, expect, beforeAll, beforeEach } from '@jest/globals';
 import { algorandFixture } from '@algorandfoundation/algokit-utils/testing';
 import * as algokit from '@algorandfoundation/algokit-utils';
+
 import { CompXStakingClient } from '../contracts/clients/CompXStakingClient';
 
 const fixture = algorandFixture();
@@ -28,23 +29,27 @@ describe('CompXStaking', () => {
       },
       algorand.client.algod
     );
+
     const stakeAssetCreate = algorand.send.assetCreate({
       sender: admin,
-      total: 10n,
+      total: 999_999_999_000n,
+      decimals: 6,
     });
     stakedAssetId = BigInt((await stakeAssetCreate).confirmation.assetIndex!);
     const rewardAssetCreate = algorand.send.assetCreate({
       sender: admin,
-      total: 10n,
+      total: 500_000_000_000n,
+      decimals: 6,
     });
+
     rewardAssetId = BigInt((await rewardAssetCreate).confirmation.assetIndex!);
 
     await appClient.create.createApplication({
       stakedAsset: stakedAssetId,
       rewardAsset: rewardAssetId,
-      minLockUp: 1,
+      minLockUp: 5,
+      contractDuration: 75,
       oracleAppID: 159512493,
-      contractDuration: 161000,
     });
   });
 
@@ -80,15 +85,15 @@ describe('CompXStaking', () => {
       sender: admin,
       receiver: appAddress,
       assetId: rewardAssetId,
-      amount: 1n,
+      amount: 100_000_000_000n,
       extraFee: algokit.algos(0.1),
     });
-    await appClient.addRewards({ rewardTxn: axferTxn, quantity: 1n });
+    await appClient.addRewards({ rewardTxn: axferTxn, quantity: 100_000_000_000n });
     const { balance: rewardAssetBalance } = await algorand.account.getAssetInformation(appAddress, rewardAssetId);
-    expect(rewardAssetBalance).toBe(1n);
+    expect(rewardAssetBalance).toBe(100_000_000_000n);
   });
 
-  test('opt in to application', async () => {
+  test('opt in to application ', async () => {
     await appClient.optIn.optInToApplication({});
     const localState = await appClient.getLocalState(admin);
     expect(localState.staked!.asBigInt()).toBe(0n);
@@ -96,33 +101,82 @@ describe('CompXStaking', () => {
     expect(localState.stakeStartTime!.asBigInt()).toBe(0n);
   });
 
-  test('stake tokens', async () => {
+  test('opt in, stake tokens, unstake and check rewards', async () => {
     const { algorand } = fixture;
+
     const { appAddress } = await appClient.appClient.getAppReference();
+    const stakerAccount = await fixture.context.generateAccount({ initialFunds: algokit.algos(10) });
+
+    console.log('staker account', stakerAccount.addr);
+    console.log('admin account', admin);
+    console.log('app address', appAddress);
+
+    await algorand.send.assetTransfer({
+      sender: stakerAccount.addr,
+      receiver: stakerAccount.addr,
+      assetId: stakedAssetId,
+      amount: 0n,
+    });
+    await algorand.send.assetTransfer({
+      sender: stakerAccount.addr,
+      receiver: stakerAccount.addr,
+      assetId: rewardAssetId,
+      amount: 0n,
+    });
+
+    await appClient.optIn.optInToApplication({}, { sender: stakerAccount });
+
+    await algorand.send.assetTransfer({
+      sender: admin,
+      receiver: stakerAccount.addr,
+      assetId: stakedAssetId,
+      amount: 300_000_000n,
+    });
 
     const axferTxn = await algorand.transactions.assetTransfer({
-      sender: admin,
+      sender: stakerAccount.addr,
       receiver: appAddress,
       assetId: stakedAssetId,
-      amount: 1_000_000n,
+      amount: 100_000_000n,
       extraFee: algokit.algos(0.1),
     });
-    await appClient.stake({ stakeTxn: axferTxn, quantity: 1, lockPeriod: 30 });
+    await appClient.stake({ stakeTxn: axferTxn, quantity: 100_000_000n, lockPeriod: 5 }, { sender: stakerAccount });
 
     const stakedAmount = (await appClient.getGlobalState()).totalStaked!.asBigInt();
-    expect(stakedAmount).toBe(1n);
-    const localState = await appClient.getLocalState(admin);
-    expect(localState.staked!.asBigInt()).toBe(1n);
+    expect(stakedAmount).toBe(100_000_000n);
+    let localState = await appClient.getLocalState(stakerAccount);
+    expect(localState.staked!.asBigInt()).toBe(100_000_000n);
+
+    // eslint-disable-next-line no-promise-executor-return
+    await new Promise((r) => setTimeout(r, 6000));
+    const { balance: preRewardBalance } = await algorand.account.getAssetInformation(stakerAccount.addr, rewardAssetId);
+    console.log('preRewardBalance', preRewardBalance);
+
+    await appClient.calculateRewards({}, { sender: stakerAccount });
+    localState = await appClient.getLocalState(stakerAccount);
+    console.log('localState.calculatedReward', localState.calculatedReward!.asBigInt());
+
+    const stakedAmountBefore = (await appClient.getGlobalState()).totalStaked!.asBigInt();
+    await appClient.unstake({}, { sendParams: { fee: algokit.algos(0.2) }, sender: stakerAccount });
+
+    const stakedAmountAfter = (await appClient.getGlobalState()).totalStaked!.asBigInt();
+    expect(stakedAmountBefore).toBe(100_000_000n);
+    expect(stakedAmountAfter).toBe(0n);
+
+    const { balance: rewardAssetBalance } = await algorand.account.getAssetInformation(
+      stakerAccount.addr,
+      rewardAssetId
+    );
+    console.log('rewardAssetBalance', rewardAssetBalance);
+
+    const { balance: stakedAssetBalance } = await algorand.account.getAssetInformation(
+      stakerAccount.addr,
+      stakedAssetId
+    );
+    expect(stakedAssetBalance).toBe(300_000_000n);
   });
 
-  test('unstake tokens', async () => {
-    const { algorand } = fixture;
-    const stakedAmountBefore = (await appClient.getGlobalState()).totalStaked!.asBigInt();
-    await appClient.unstake({});
-    const stakedAmountAfter = (await appClient.getGlobalState()).totalStaked!.asBigInt();
-    expect(stakedAmountBefore).toBe(1n);
-    expect(stakedAmountAfter).toBe(0n);
-    const { balance: stakedAssetBalance } = await algorand.account.getAssetInformation(admin, stakedAssetId);
-    expect(stakedAssetBalance).toBe(10n);
+  test('deleteApplication', async () => {
+    await appClient.delete.deleteApplication({});
   });
 });
--- contracts/projects/contracts/contracts/CompXStaking.algo.ts
+++ contracts/projects/contracts/contracts/CompXStaking.algo.ts
@@ -15,9 +15,13 @@ export class CompXStaking extends Contract {
 
   contractDuration = GlobalStateKey<uint64>();
 
+  calculatedReward = LocalStateKey<uint64>();
+
   staked = LocalStateKey<uint64>();
 
   unlockTime = LocalStateKey<uint64>();
+
+  stakeDuration = LocalStateKey<uint64>();
 
   stakeStartTime = LocalStateKey<uint64>();
 
@@ -41,6 +45,7 @@ export class CompXStaking extends Contract {
     this.staked(this.txn.sender).value = 0;
     this.unlockTime(this.txn.sender).value = 0;
     this.stakeStartTime(this.txn.sender).value = 0;
+    this.stakeDuration(this.txn.sender).value = 0;
   }
 
   optInToAsset(mbrTxn: PayTxn): void {
@@ -69,7 +74,7 @@ export class CompXStaking extends Contract {
     }
   }
 
-  updateParams(minLockUp: uint64, maxLockUp: uint64, oracleAppID: AppID, contractDuration: uint64): void {
+  updateParams(minLockUp: uint64, oracleAppID: AppID, contractDuration: uint64): void {
     assert(this.txn.sender === this.app.creator);
 
     this.minLockUp.value = minLockUp;
@@ -112,42 +117,83 @@ export class CompXStaking extends Contract {
     this.totalStaked.value += quantity;
     this.staked(this.txn.sender).value += quantity;
     this.stakeStartTime(this.txn.sender).value = globals.latestTimestamp;
+    this.stakeDuration(this.txn.sender).value = lockPeriod;
 
-    this.unlockTime(this.txn.sender).value = globals.latestTimestamp + lockPeriod * 86400;
+    this.unlockTime(this.txn.sender).value = globals.latestTimestamp + lockPeriod;
   }
 
-/*   getOraclePrice(token: AssetID): uint64 {
+  /*   getOraclePrice(token: AssetID): uint64 {
     const tokenPrice = this.oracleAppID.value.globalState(itob(token.id)) as BytesLike;
     return btoi(tokenPrice);
   } */
 
+  calculateRewards(): void {
+    const quantity = this.staked(this.txn.sender).value;
+    assert(quantity > 0, 'No staked assets');
+    // assert(this.unlockTime(this.txn.sender).value < globals.latestTimestamp, 'unlock time not reached'); // add in this check
+
+    const stakingDuration = this.stakeDuration(this.txn.sender).value;
+    const stakeAmount = this.staked(this.txn.sender).value;
+
+    const stakeTokenPrice = 1000000; // this.getOraclePrice(this.stakedAssetId.value);
+    const rewardTokenPrice = 150000; // this.getOraclePrice(this.rewardAssetId.value);
+
+    const stakedAmountlowerPrecision = stakeAmount / 10 ** 4;
+    const stakeTokenPriceLowerPrecision = stakeTokenPrice / 10 ** 4;
+    const rewardTokenPriceLowerPrecision = rewardTokenPrice / 10 ** 4;
+    const totalRewardsLowerPrecision = this.totalRewards.value / 10 ** 4;
+    const totalStakedLowerPrecision = this.totalStaked.value / 10 ** 4;
+
+    const normalisedAmount =
+      (stakedAmountlowerPrecision * stakeTokenPriceLowerPrecision) / rewardTokenPriceLowerPrecision;
+    const rewardNom = normalisedAmount * stakingDuration * totalRewardsLowerPrecision;
+    const rewardDom =
+      (totalStakedLowerPrecision * stakeTokenPriceLowerPrecision * this.contractDuration.value) /
+      rewardTokenPriceLowerPrecision;
+    this.calculatedReward(this.txn.sender).value = (rewardNom / rewardDom) * 10 ** 4;
+  }
+
   unstake(): void {
     const quantity = this.staked(this.txn.sender).value;
     assert(quantity > 0, 'No staked assets');
-    assert(this.unlockTime(this.txn.sender).value < globals.latestTimestamp, 'unlock time not reached'); // add in this check
+    // assert(this.unlockTime(this.txn.sender).value < globals.latestTimestamp, 'unlock time not reached'); // add in this check
 
-    const stakingDuration = globals.latestTimestamp - this.stakeStartTime(this.txn.sender).value;
+    const stakingDuration = this.stakeDuration(this.txn.sender).value;
     const stakeAmount = this.staked(this.txn.sender).value;
 
-    const stakeTokenPrice = 1_000_000; // this.getOraclePrice(this.stakedAssetId.value);
-    const rewardTokenPrice = 140_000; // this.getOraclePrice(this.rewardAssetId.value);
+    const stakeTokenPrice = 1000000; // this.getOraclePrice(this.stakedAssetId.value);
+    const rewardTokenPrice = 150000; // this.getOraclePrice(this.rewardAssetId.value);
 
-    const normalisedAmount = (stakeAmount * stakeTokenPrice) / rewardTokenPrice;
+    // lower precision for calculation
+    const stakedAmountlowerPrecision = stakeAmount / 10 ** 4;
+    const stakeTokenPriceLowerPrecision = stakeTokenPrice / 10 ** 4;
+    const rewardTokenPriceLowerPrecision = rewardTokenPrice / 10 ** 4;
+    const totalRewardsLowerPrecision = this.totalRewards.value / 10 ** 4;
+    const totalStakedLowerPrecision = this.totalStaked.value / 10 ** 4;
+
+    /* const normalisedAmount = (stakeAmount * stakeTokenPrice) / rewardTokenPrice;
     const reward =
       (normalisedAmount * stakingDuration * this.totalRewards.value) /
-      ((this.totalStaked.value * stakeTokenPrice * this.contractDuration.value) / rewardTokenPrice);
+      ((this.totalStaked.value * stakeTokenPrice * this.contractDuration.value) / rewardTokenPrice); */
+    // 100 * 5 * 1234 / 100 * 1 * 75 / 1
+    // 617,000 / (7500) / 1
+    const normalisedAmount =
+      (stakedAmountlowerPrecision * stakeTokenPriceLowerPrecision) / rewardTokenPriceLowerPrecision;
+    const rewardNom = normalisedAmount * stakingDuration * totalRewardsLowerPrecision;
+    const rewardDom =
+      (totalStakedLowerPrecision * stakeTokenPriceLowerPrecision * this.contractDuration.value) /
+      rewardTokenPriceLowerPrecision;
+    const reward = (rewardNom / rewardDom) * 10 ** 4;
 
     sendAssetTransfer({
       xferAsset: this.stakedAssetId.value,
       assetReceiver: this.txn.sender,
       assetAmount: quantity,
-      assetCloseTo: this.txn.sender,
     });
     sendAssetTransfer({
       xferAsset: this.rewardAssetId.value,
       assetReceiver: this.txn.sender,
       assetAmount: reward,
-      assetCloseTo: this.txn.sender,
     });
 
     this.totalStaked.value -= quantity;
